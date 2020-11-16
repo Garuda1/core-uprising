12be0671051afde170696594282f80d4967a75db
Garuda1 <sashamurgia@protonmail.ch>
2020-11-16 12:45:19 +0100

    wtf

--- headers/text.h
+++ headers/text.h
@@ -10,14 +10,15 @@
 #define COLOUR_RED   "\x1B[31m"
 #define TEXT_FAILURE "[" COLOUR_RED "FAILURE" COLOUR_RESET "] "
 
-#define TEXT_ARGS_INVALIDARG    TEXT_FAILURE "args_parse: invalid argument (%s)"
-#define TEXT_ARGS_PARSE_FAILURE TEXT_FAILURE "args_parse: failed to parse argument"
-#define TEXT_ARGS_CHECK_FAILURE TEXT_FAILURE "args_check: invalid arguments"
+#define TEXT_ARGS_INVALIDARG           TEXT_FAILURE "args_parse: invalid argument (%s)"
+#define TEXT_ARGS_PARSE_FAILURE        TEXT_FAILURE "args_parse: failed to parse argument"
+#define TEXT_ARGS_PARSE_MALLOC_FAILURE TEXT_FAILURE "args_parse: failled to malloc"
+#define TEXT_ARGS_CHECK_FAILURE        TEXT_FAILURE "args_check: invalid arguments"
 
-#define TEXT_MAIN_FAILURE       TEXT_FAILURE "core-uprising failed to execute properly"
+#define TEXT_MAIN_FAILURE              TEXT_FAILURE "core-uprising failed to execute properly"
 
-#define TEXT_GAME_INIT_FAILURE  TEXT_FAILURE "game_init: failed to initialize the s_game structure"
+#define TEXT_GAME_INIT_FAILURE         TEXT_FAILURE "game_init: failed to initialize the s_game structure"
 
-#define TEXT_VM_EXEC_HCF        TEXT_FAILURE "vm_exec: MACHINE HALTED AND CAUGHT ON FIRE ! AAAAAAAAAAH"
+#define TEXT_VM_EXEC_HCF               TEXT_FAILURE "vm_exec: MACHINE HALTED AND CAUGHT ON FIRE ! AAAAAAAAAAH"
 
 #endif
--- sources/args.c
+++ sources/args.c
@@ -29,12 +29,15 @@ t_args *args_parse(t_args *args, const int argc, char **argv)
   current_player = 0;
 
   /* For each shell argument */
-  for (i=0; i<argc; ++i)
+  for (i=1; i<argc; ++i)
   {
+    printf("argc=%d\n", argc);
     if ((!strcmp(argv[i], FLAG_PLAYER_NB) || !strcmp(argv[i], FLAG_ALT_PLAYER_NB))  && (i+1)<argc)
     {
       args->player_nb = strtoul(argv[++i], NULL, 10);
-      args->player_path = malloc(sizeof(char*)*(args->player_nb));
+      printf("player_nb=%ld\n", args->player_nb);
+      if ((args->player_path = malloc(sizeof(char*)*(args->player_nb))) == NULL)
+        return (retstr(NULL, TEXT_ARGS_PARSE_MALLOC_FAILURE, __FILE__, __LINE__));
     }
 
     else if ((!strcmp(argv[i], FLAG_PLAYER_SIZE) || !strcmp(argv[i], FLAG_ALT_PLAYER_SIZE)) && (i+1)<argc)
